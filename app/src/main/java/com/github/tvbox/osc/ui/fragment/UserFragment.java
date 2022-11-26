3b5c6edd4e1befbc7baa1d8a4d8161ecb047f3f9
汪振 <137014311@qq.com>
2022-11-26 21:14:16 +0800

    feat:init

---
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/UserFragment.java
@@ -0,0 +1,206 @@
+package com.github.tvbox.osc.ui.fragment;
+
+import android.content.Intent;
+import android.view.View;
+import android.view.animation.BounceInterpolator;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.github.tvbox.osc.R;
+import com.github.tvbox.osc.api.ApiConfig;
+import com.github.tvbox.osc.base.BaseLazyFragment;
+import com.github.tvbox.osc.bean.Movie;
+import com.github.tvbox.osc.event.ServerEvent;
+import com.github.tvbox.osc.ui.activity.SearchActivity;
+import com.github.tvbox.osc.ui.adapter.HomeHotVodAdapter;
+import com.github.tvbox.osc.util.FastClickCheckUtil;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.AbsCallback;
+import com.lzy.okgo.model.Response;
+import com.orhanobut.hawk.Hawk;
+import com.owen.tvrecyclerview.widget.TvRecyclerView;
+import com.owen.tvrecyclerview.widget.V7GridLayoutManager;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * @author pj567
+ * @date :2021/3/9
+ * @description:
+ */
+public class UserFragment extends BaseLazyFragment implements View.OnClickListener {
+    private TvRecyclerView tvHotList;
+    private HomeHotVodAdapter homeHotVodAdapter;
+    private List<Movie.Video> homeSourceRec;
+
+    public static UserFragment newInstance() {
+        return new UserFragment();
+    }
+
+    public static UserFragment newInstance(List<Movie.Video> recVod) {
+        return new UserFragment().setArguments(recVod);
+    }
+
+    public UserFragment setArguments(List<Movie.Video> recVod) {
+        this.homeSourceRec = recVod;
+        return this;
+    }
+
+    @Override
+    protected void onFragmentResume() {
+        //多行显示
+        tvHotList.setHasFixedSize(true);
+        V7GridLayoutManager v7Manager = new V7GridLayoutManager(this.mContext, 4);
+        tvHotList.setLayoutManager(v7Manager);
+        v7Manager.setSpanSizeLookup(new V7GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (position == 0||position == 1){
+                    return 2;
+                }
+                return 1;
+            }
+        });
+        super.onFragmentResume();
+    }
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.fragment_user;
+    }
+
+    @Override
+    protected void init() {
+        EventBus.getDefault().register(this);
+        tvHotList = findViewById(R.id.tvHotList);
+        homeHotVodAdapter = new HomeHotVodAdapter();
+        homeHotVodAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                if (ApiConfig.get().getSourceBeanList().isEmpty())
+                    return;
+                Movie.Video vod = ((Movie.Video) adapter.getItem(position));
+                Intent newIntent = new Intent(mContext, SearchActivity.class);
+                newIntent.putExtra("title", vod.name);
+                newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                mActivity.startActivity(newIntent);
+            }
+        });
+        tvHotList.setOnItemListener(new TvRecyclerView.OnItemListener() {
+            @Override
+            public void onItemPreSelected(TvRecyclerView parent, View itemView, int position) {
+                itemView.animate().scaleX(1.0f).scaleY(1.0f).setDuration(500).setInterpolator(new BounceInterpolator()).start();
+            }
+
+            @Override
+            public void onItemSelected(TvRecyclerView parent, View itemView, int position) {
+                itemView.animate().scaleX(1.05f).scaleY(1.05f).setDuration(500).setInterpolator(new BounceInterpolator()).start();
+            }
+
+            @Override
+            public void onItemClick(TvRecyclerView parent, View itemView, int position) {
+
+            }
+        });
+        tvHotList.setAdapter(homeHotVodAdapter);
+
+        initHomeHotVod(homeHotVodAdapter);
+    }
+
+    private void initHomeHotVod(HomeHotVodAdapter adapter) {
+        try {
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH) + 1;
+            int day = cal.get(Calendar.DATE);
+            String today = String.format("%d%d%d", year, month, day);
+            String requestDay = Hawk.get("home_hot_day", "");
+            //每天只请求一次
+            if (requestDay.equals(today)) {
+                String json = Hawk.get("home_hot", "");
+                if (!json.isEmpty()) {
+                    adapter.setNewData(loadHots(json));
+                    return;
+                }
+            }
+//            OkGo.<String>get("https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=&playable=1&start=0&year_range=" + year + "," + year).execute(new AbsCallback<String>() {
+            OkGo.<String>get("https://api.coder.wang/douban/mock").execute(new AbsCallback<String>() {
+                @Override
+                public void onSuccess(Response<String> response) {
+                    String netJson = response.body();
+                    Hawk.put("home_hot_day", today);
+                    Hawk.put("home_hot", netJson);
+                    mActivity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.setNewData(loadHots(netJson));
+                        }
+                    });
+                }
+
+                @Override
+                public String convertResponse(okhttp3.Response response) throws Throwable {
+                    return response.body().string();
+                }
+            });
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+    }
+
+    private ArrayList<Movie.Video> loadHots(String json) {
+        ArrayList<Movie.Video> result = new ArrayList<>();
+        try {
+            JsonObject infoJson = new Gson().fromJson(json, JsonObject.class);
+            JsonArray array = infoJson.getAsJsonArray("data");
+            for (JsonElement ele : array) {
+                JsonObject obj = (JsonObject) ele;
+                Movie.Video vod = new Movie.Video();
+                vod.name = obj.get("title").getAsString();
+                vod.note = obj.get("updateInfo").getAsString();
+                vod.pic = obj.get("photos").getAsString();
+                result.add(vod);
+            }
+        } catch (Throwable th) {
+
+        }
+        return result;
+    }
+
+    private View.OnFocusChangeListener focusChangeListener = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            if (hasFocus)
+                v.animate().scaleX(1.05f).scaleY(1.05f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
+            else
+                v.animate().scaleX(1.0f).scaleY(1.0f).setDuration(300).setInterpolator(new BounceInterpolator()).start();
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        FastClickCheckUtil.check(v);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void server(ServerEvent event) {
+        if (event.type == ServerEvent.SERVER_CONNECTION) {
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        EventBus.getDefault().unregister(this);
+    }
+}
